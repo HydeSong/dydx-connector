05aed368112ecdc418845e343c769067f76bf2f0
Hyde Song <hyde@Hydes-MacBook-Pro.local>
2023-06-13 15:48:13 +0800

    material

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+export const footerLinks = [
+    {
+        title: "About",
+        links: [
+            { title: "How it works", url: "/", icon: ""  },
+            { title: "Featured", url: "/", icon: ""  },
+            { title: "Partnership", url: "/", icon: ""  },
+            { title: "Bussiness Relation", url: "/", icon: ""  },
+        ],
+    },
+    {
+        title: "Company",
+        links: [
+            { title: "Events", url: "/", icon: ""  },
+            { title: "Blog", url: "/", icon: ""  },
+            { title: "Podcast", url: "/", icon: ""  },
+            { title: "Invite a friend", url: "/", icon: ""  },
+        ],
+    },
+    {
+        title: "Socials",
+        links: [
+            { title: "Discord", url: "/", icon: "/discord.svg" },
+            { title: "Instagram", url: "/", icon: "/instagram.svg"  },
+            { title: "Twitter", url: "/", icon: "/twitter.svg"  },
+            { title: "Facebook", url: "/", icon: "/facebook.svg"  },
+        ],
+    },
+];
